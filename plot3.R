2f45e852338ba97c4460172368701b7ebcb28b32
0daven <poncho.loa@gmail.com>
2018-01-13 19:51:24 +0000

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -18,3 +18,6 @@ with(sub.pow,{
 legend("topright", lty=1, col=c("black","red","blue"),legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
 #Addin title
 title(main="Energy sub-metering")
+## Save file and close device
+#dev.copy(png,"plot3.png", width=480, height=480)
+#dev.off()
